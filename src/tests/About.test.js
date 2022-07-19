c99a864759f3888f7b0c37657f28232b70820400
Melania P Chagas <melaniapchagas@gmail.com>
2022-07-19 17:36:53 -0300

    Requisito 2 - Teste 1/3: Verifica se a página contém um heading h2 com o texto 'About Pokédex'

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testes acerca da página \'About\'',
+  () => {
+    test('Teste se a página contém um heading h2 com o texto \'About Pokédex\'', () => {
+      renderWithRouter(<App />);
+      const linkAbout = screen.getByRole('link', { name: /about/i });
+      userEvent.click(linkAbout);
+      const heading2 = screen.getByRole('heading', { name: /About Pokédex/i });
+
+      expect(heading2).toHaveTextContent(/About Pokédex/i);
+    });
+  });
