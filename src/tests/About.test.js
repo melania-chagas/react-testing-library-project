dcea345f80594813d7788cc7c0f210fa549e4532
Melania P Chagas <melaniapchagas@gmail.com>
2022-07-19 19:23:38 -0300

    Requisito 2 - Correções de código

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-
-import App from '../App';
-import renderWithRouter from './renderWithRouter';
+import { render, screen } from '@testing-library/react';
+import { About } from '../pages';
 
 describe('Testes acerca da página \'About\'',
   () => {
     test('Teste se a página contém um heading h2 com o texto \'About Pokédex\'', () => {
-      renderWithRouter(<App />);
-      const linkAbout = screen.getByRole('link', { name: /about/i });
-      userEvent.click(linkAbout);
+      render(<About />);
       const heading2 = screen.getByRole('heading', { name: /About Pokédex/i });
 
       expect(heading2).toHaveTextContent(/About Pokédex/i);
@@ -18,9 +13,7 @@ describe('Testes acerca da página \'About\'',
 
     test('Teste se a página contém dois parágrafos com texto sobre a Pokédex',
       () => {
-        renderWithRouter(<App />);
-        const linkAbout = screen.getByRole('link', { name: /about/i });
-        userEvent.click(linkAbout);
+        render(<About />);
 
         expect(screen.getByText(
           /This application simulates a Pokédex/i,
@@ -33,12 +26,9 @@ describe('Testes acerca da página \'About\'',
 
     test('Testa se existe uma imagem específica',
       () => {
-        renderWithRouter(<App />);
-        const linkAbout = screen.getByRole('link', { name: /about/i });
-        userEvent.click(linkAbout);
-
-        expect(screen.getByAltText(
-          /Pokédex/i,
-        )).toBeInTheDocument();
+        render(<About />);
+        const img = screen.getByRole('img', { name: /Pokédex/i });
+        const urlImg = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+        expect(img).toHaveAttribute('src', urlImg);
       });
   });
