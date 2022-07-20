2c90029fab18dfa0861cdfebdc534a932f71e600
Melania P Chagas <melaniapchagas@gmail.com>
2022-07-20 09:45:51 -0300

    Requisito 3 - Verifica se é exibida na tela a mensagem 'No favorite pokemon found', caso a pessoa não tenha pokémons favoritos

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+
+describe('Testes acerca da página \'FavoritePokemons\'', () => {
+  test(
+    `Verifica se é exibida na tela a mensagem 'No favorite pokemon found',
+      caso a pessoa não tenha pokémons favoritos`,
+    () => {
+      render(<FavoritePokemons />);
+      const noFavorites = screen.getByText(/ favorite pokemon found/i);
+      expect(noFavorites).toBeInTheDocument();
+    },
+  );
+});
